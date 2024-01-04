c3ff13238791c22333a09e258748841d416f2e2c
Ahmad-server <pouramini@gmail.com>
2024-01-04 19:06:26 +0330

    commit

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'curses==2.2.0'
-        'pyperclip'
-        'textwrap'
+        'curses==2.2.0',
+        'pyperclip',
+        'textwrap',
     ],
 )
 
