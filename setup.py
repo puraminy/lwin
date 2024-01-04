1a1fe1f3edcc2df0da80d6effed1dd35f40c3116
Ahmad-server <pouramini@gmail.com>
2024-01-04 19:28:00 +0330

    commit

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup, find_packages
+"""Install Compacter."""
+import os
+import setuptools
 
-setup(
-    name='lwin',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'pyperclip',
-        'textwrap3',
-    ],
-)
 
+def setup_package():
+    long_description = "mwin"
+    setuptools.setup(
+        name='mwin',
+        description='mwin',
+        version='0.0.1',
+        long_description=long_description,
+        license='MIT License',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'textwrap3',
+            'pyperclip'
+        ],
+    )
+
+if __name__ == '__main__':
+    setup_package()
